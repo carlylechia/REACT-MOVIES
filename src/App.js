c2cb9c02bb72cdad75a7c94e35f6d5153f6c41e2
Chia Carlyle <chiacarlyle@gmail.com>
2022-05-06 17:40:41 +0100

    Fetch API and display static data for single movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from "react";
+import { useEffect } from "react";
 import './App.css';
+import searchicon from './search.svg';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=7b0d80cb';
+
+const App = () => {
+  const loadMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  }
+  useEffect(() => {
+    loadMovies('superman')
+  }, [])
+
+  const Movie1 = {
+    "Title": "Superman/Batman: Public Enemies",
+    "Year": "2009",
+    "imdbID": "tt1398941",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZDc5NTFiMzgtZWJiOS00N2M1LWJmOGYtZmNjMzFhMzcxZjRiXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value={'superman'}
+          onChange{...() => {}}
+        />
+        <img
+          src={searchicon}
+          alt="search"
+          onClick={() => {}}
+        />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+          <p>{Movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={Movie1.Poster !== 'N/A' ? Movie1.Poster : 'https://via.placeholder.com/400'}
+              alt={Movie1.Title}
+            />
+          </div>
+          <div>
+            <span>{Movie1.Type}</span>
+            <h3>{Movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals(console.log);
+ReactDOM.render(<App />, document.getElementById('root'));
